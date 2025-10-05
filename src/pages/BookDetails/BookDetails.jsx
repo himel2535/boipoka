e70fccad3f1bbabf9000fe099f7c8e7715d336eb
himel2535 <monwarhossanhimel@gmail.com>
2025-10-06 01:18:48 +0600

    Store wishlist books to Local Storage and show toast alert

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
+import { addToStoreDB } from "../../utility/addToDB";
 
 const BookDetails = () => {
+
   const { id } = useParams();
   const parsedId = parseInt(id);
-  // console.log(id)
   const data = useLoaderData();
   const singleBook = data.find((book) => book.bookId === parsedId);
   // console.log(singleBook)
   const { bookName, image } = singleBook;
+
+    const handleMarkAsRead=id=>{
+        addToStoreDB(id)
+    }
+
   return (
     <div className="flex flex-col items-center justify-center mx-auto my-10">
       <img className="w-[200px] h-[300px]" src={image} alt="" />
       <h1>{bookName}</h1>
       <div>
-        <button className="btn btn-accent m-2">Accent</button>
-        <button className="btn btn-info m-2">Info</button>
+        <button onClick={()=>handleMarkAsRead(id)} className="btn btn-accent m-2">Mark as List</button>
+        <button className="btn btn-info m-2">Add to WishList</button>
       </div>
     </div>
   );
