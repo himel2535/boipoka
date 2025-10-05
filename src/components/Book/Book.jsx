7a4bbed8c34b7aebabbde7d209f2d12d47af642c
himel2535 <monwarhossanhimel@gmail.com>
2025-10-05 19:44:20 +0600

    Polishing the product card and adding React Icons

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,20 +1,18 @@
 import React from "react";
+import { FaStarHalfAlt } from "react-icons/fa";
 
 const Book = ({ singleBook }) => {
-//   console.log(singleBook);
-    const {bookName,author,image}=singleBook
+  //   console.log(singleBook);
+  const { bookName, author, image, publisher, rating, category } = singleBook;
 
   return (
-    <div className="card bg-base-100 w-96 shadow-sm">
-      <figure>
-        <img
-          src={image}
-          alt="Shoes"
-        />
+    <div className="card bg-base-100 w-96 shadow-sm border">
+      <figure className="bg-gray-100 w-10/11  mx-auto my-3">
+        <img className="h-[166px] p-3 rounded-2xl" src={image} alt="Shoes" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">
-          Card Title
+          {bookName}
           <div className="badge badge-secondary">NEW</div>
         </h2>
         <p>
@@ -22,8 +20,11 @@ const Book = ({ singleBook }) => {
           title and actions parts
         </p>
         <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+          <div className="badge badge-outline">{category}</div>
+          <div className="badge badge-outline">
+            {rating}
+            <FaStarHalfAlt />
+          </div>
         </div>
       </div>
     </div>
